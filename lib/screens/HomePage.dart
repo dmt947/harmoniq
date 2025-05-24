e3445596938a7b602ac8dae30559ff3511737c28
dmt947 <mot.dxn@gmail.com>
2025-05-24 04:02:34 -0600

    feat(auth): add user details after succesful login

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final user = FirebaseAuth.instance.currentUser;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bienvenido, ${user?.email ?? 'usuario'}'),
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.logout),
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+              }),
+        ],
+      ),
+      body: const Center(child: Text('Esta es la página principal')),
+    );
   }
 }
