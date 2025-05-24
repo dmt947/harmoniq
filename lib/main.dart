0877c4c93a8a0a6ca2932cdfb0f134527f4f54fe
dmt947 <mot.dxn@gmail.com>
2025-05-24 04:00:35 -0600

    style: format code

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:harmoniq/theme/harmoniq_theme.dart';
 import 'package:harmoniq/screens/AuthScreen.dart';
 
-void main(){
-    runApp(Harmoniq());
-  }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(Harmoniq());
+}
 
 class Harmoniq extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-        return MaterialApp(
-          title: 'Harmoniq',
-          darkTheme: HarmoniqTheme.darkTheme,
-          theme: HarmoniqTheme.lightTheme,
-          themeMode: ThemeMode.system,
-          home: AuthScreen()
-        );
-      }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Harmoniq',
+      darkTheme: HarmoniqTheme.darkTheme,
+      theme: HarmoniqTheme.lightTheme,
+      themeMode: ThemeMode.system,
+      home: AuthScreen(),
+    );
   }
-
+}
